0f5070b8aa78a4f69bd2a5d131a984e4959efd60
lfoke1234 <lovejh001216@gmail.com>
2024-08-18 00:00:43 +0900

    fix bug

--- ATwilightFixer/Assets/Scripts/Puzzle/Puzzle_SequentialButton.cs
+++ ATwilightFixer/Assets/Scripts/Puzzle/Puzzle_SequentialButton.cs
@@ -2,96 +2,114 @@ using UnityEngine;
 
 public class Puzzle_SequentialButton : MonoBehaviour
 {
-    [SerializeField] private GameObject[] buttons;
-    [SerializeField] private GameObject door;
-    [SerializeField] private float distanceToPlayer;
-    [SerializeField] private GameObject textPrefab;
+    [SerializeField] private GameObject[] buttons;  // 순서대로 눌러야 하는 버튼들
+    [SerializeField] private GameObject door;  // 퍼즐이 완료되면 열리는 문
+    [SerializeField] private float distanceToPlayer;  // 플레이어와 버튼 간의 거리
+    [SerializeField] private Sprite leverOnSprite;  // 활성화된 레버 스프라이트
+    [SerializeField] private Sprite leverOffSprite; // 비활성화된 레버 스프라이트
+    [SerializeField] private GameObject finalButton; // 최종 확인 버튼
+
     private int currentButtonIndex;
     private bool isCleard;
-
-    private GameObject[] texts;
+    private bool puzzleFailed;
+    private SpriteRenderer[] buttonRenderers;
 
     private void Start()
     {
-        texts = new GameObject[buttons.Length];
+        buttonRenderers = new SpriteRenderer[buttons.Length];
         for (int i = 0; i < buttons.Length; i++)
         {
-            texts[i] = Instantiate(textPrefab, buttons[i].transform.position + Vector3.up * 2, Quaternion.identity);
-            texts[i].SetActive(false);
+            buttonRenderers[i] = buttons[i].GetComponent<SpriteRenderer>();
+            buttonRenderers[i].sprite = leverOffSprite;
         }
+        finalButton.GetComponent<SpriteRenderer>().sprite = leverOffSprite;
     }
 
     private void Update()
     {
         CheckPlayerInput();
-        UpdateTextVisibility();
-    }
-
-    private void UpdateTextVisibility()
-    {
-        if (!isCleard)
-        {
-            for (int i = 0; i < buttons.Length; i++)
-            {
-                if (IsPlayerNearby(buttons[i]))
-                {
-                    texts[i].SetActive(true);
-                }
-                else
-                {
-                    texts[i].SetActive(false);
-                }
-            }
-        }
     }
 
     private void CheckPlayerInput()
     {
-        if (IsPlayerNearby(buttons[currentButtonIndex]) && IsActionTriggered("Interaction"))
+        if (!isCleard)
         {
-            TriggerButton(buttons[currentButtonIndex]);
-        }
-        else
-        {
-            for (int i = 0; i < buttons.Length; i++)
+            // currentButtonIndex가 buttons.Length보다 크지 않도록 확인
+            if (currentButtonIndex < buttons.Length && IsPlayerNearby(buttons[currentButtonIndex]) && IsActionTriggered("Interaction"))
             {
-                if (i != currentButtonIndex && IsPlayerNearby(buttons[i]) && IsActionTriggered("Interaction"))
+                TriggerButton(currentButtonIndex);
+            }
+            else if (IsPlayerNearby(finalButton) && IsActionTriggered("Interaction"))
+            {
+                if (currentButtonIndex == buttons.Length && !puzzleFailed)
+                {
+                    OnPuzzleCompleted();
+                }
+                else
                 {
                     ResetPuzzle();
-                    break;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    if (i != currentButtonIndex && IsPlayerNearby(buttons[i]) && IsActionTriggered("Interaction"))
+                    {
+                        TriggerIncorrectButton(i);
+                        break;
+                    }
                 }
             }
         }
     }
 
-    private bool IsPlayerNearby(GameObject button)
+    private void TriggerButton(int buttonIndex)
     {
-        return Vector2.Distance(button.transform.position, PlayerManager.instance.player.transform.position) <= distanceToPlayer;
-    }
-
-    private void TriggerButton(GameObject button)
-    {
-        if (currentButtonIndex < buttons.Length - 1)
+        if (buttonIndex == currentButtonIndex)
         {
+            buttonRenderers[buttonIndex].sprite = leverOnSprite;
             currentButtonIndex++;
-            Debug.Log(currentButtonIndex);
+            Debug.Log("Button " + (buttonIndex + 1) + " activated.");
         }
         else
         {
-            OnPuzzleCompleted();
+            TriggerIncorrectButton(buttonIndex);
         }
+    }
+
+    private void TriggerIncorrectButton(int buttonIndex)
+    {
+        buttonRenderers[buttonIndex].sprite = leverOnSprite; // 틀린 레버도 스프라이트 변경
+        puzzleFailed = true;
+        Debug.Log("Puzzle failed!");
     }
 
     private void OnPuzzleCompleted()
     {
         door.SetActive(false);
         isCleard = true;
+        finalButton.GetComponent<SpriteRenderer>().sprite = leverOnSprite;
+        Debug.Log("Puzzle completed!");
     }
 
     private void ResetPuzzle()
     {
         currentButtonIndex = 0;
-        Debug.Log("땡!");
+        puzzleFailed = false;
+        Debug.Log("Resetting puzzle.");
+
+        // 모든 레버의 스프라이트를 원래대로 돌림
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttonRenderers[i].sprite = leverOffSprite;
+        }
+        finalButton.GetComponent<SpriteRenderer>().sprite = leverOffSprite;
+    }
+
+    private bool IsPlayerNearby(GameObject button)
+    {
+        return Vector2.Distance(button.transform.position, PlayerManager.instance.player.transform.position) <= distanceToPlayer;
     }
 
     public bool IsActionTriggered(string actionName)
